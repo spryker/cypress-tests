5c2673990131c17a1e3a972a2b8b248adbe872d1
Dmytro Asieiev <dmiseev@gmail.com>
2023-11-26 22:47:08 +0300

    Added dummy scenarios.

---
+++ cypress/support/scenarios/create.customer.scenario.ts
@@ -0,0 +1,5 @@
+export class CreateCustomerScenario {
+    execute = () => {
+        // TODO;
+    }
+}
---
+++ cypress/support/scenarios/place.order.scenario.ts
@@ -0,0 +1,25 @@
+import {CartPage} from "../pages/cart/cart.page";
+import {AddressPage} from "../pages/checkout/address/address.page";
+import {ShipmentPage} from "../pages/checkout/shipment/shipment.page";
+import {PaymentPage} from "../pages/checkout/payment/payment.page";
+import {SummaryPage} from "../pages/checkout/summary/summary.page";
+
+export class PlaceOrderScenario {
+    execute = () => {
+        const cartPage = new CartPage();
+        const addressStepPage = new AddressPage();
+        const shipmentStepPage = new ShipmentPage();
+        const paymentStepPage = new PaymentPage();
+        const summaryStepPage = new SummaryPage();
+
+        cy.visit(cartPage.getPageLocation());
+        cartPage.quickAddToCart('169_25880805', 1);
+        cartPage.quickAddToCart('156_32018944', 2);
+
+        cartPage.startCheckout();
+        addressStepPage.fillShippingAddress();
+        shipmentStepPage.setStandardShippingMethod();
+        paymentStepPage.setDummyPaymentMethod();
+        summaryStepPage.placeOrder();
+    }
+}
