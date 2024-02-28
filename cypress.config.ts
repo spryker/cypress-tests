f95b82801c8a07d31404a2121473934febf5b854
Denys Sokolov <denys.sokolov@spryker.com>
2024-02-28 12:24:25 +0100

    CC-32482: Updated the configuration of the cypress.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,30 +1,42 @@
-import { defineConfig } from 'cypress';
+import {defineConfig} from 'cypress';
 import dotenv from 'dotenv';
 import * as fs from 'fs';
+
 dotenv.config();
 
+function getEnvVar(primary: string, fallback: string): string {
+    return process.env[primary] || process.env[fallback];
+}
+
+const protocol = getEnvVar('ENV_PROTOCOL', 'SPRYKER_SSL_ENABLE') ? 'https' : 'http';
+const backofficeHost = getEnvVar('ENV_BACKOFFICE_HOST', 'SPRYKER_BE_HOST');
+const merchantPortalHost = getEnvVar('ENV_MERCHANT_PORTAL_HOST', 'SPRYKER_MP_HOST');
+const glueBackendHost = getEnvVar('ENV_GLUE_BACKEND_HOST', 'SPRYKER_GLUE_BACKEND_HOST');
+const mailCatcherHost = getEnvVar('ENV_MAIL_CATCHER_HOST', 'SPRYKER_SMTP_HOST');
+const baseHost = getEnvVar('E2E_BASE_HOST', 'SPRYKER_FE_HOST');
+
 export default defineConfig({
-  env: {
-    repositoryId: process.env.ENV_REPOSITORY_ID,
-    backofficeUrl: process.env.ENV_BACKOFFICE_URL,
-    merchantPortalUrl: process.env.ENV_MERCHANT_PORTAL_URL,
-    glueBackendUrl: process.env.ENV_GLUE_BACKEND_URL,
-    mailCatcherUrl: process.env.ENV_MAIL_CATCHER_URL,
-  },
-  e2e: {
-    baseUrl: process.env.E2E_BASE_URL,
-    setupNodeEvents(on) {
-      on('task', {
-        isFileExists(filename): boolean {
-          return fs.existsSync(filename);
+    env: {
+        repositoryId: process.env.ENV_REPOSITORY_ID,
+        backofficeUrl: `${protocol}://${backofficeHost}`,
+        merchantPortalUrl: `${protocol}://${merchantPortalHost}`,
+        glueBackendUrl: `${protocol}://${glueBackendHost}`,
+        mailCatcherUrl: `${protocol}://${mailCatcherHost}`,
+    },
+    e2e: {
+        baseUrl: `${protocol}://${baseHost}`,
+        setupNodeEvents(on) {
+            on('task', {
+                isFileExists(filename: string): boolean {
+                    return fs.existsSync(filename);
+                },
+            });
         },
-      });
+        retries: {
+            runMode: 2,
+            openMode: 0,
+        },
     },
-    retries: {
-      runMode: 2,
-      openMode: 0,
-    },
-  },
-  viewportWidth: parseInt(process.env.VIEWPORT_WIDGTH ?? '1000', 10),
-  viewportHeight: parseInt(process.env.VIEWPORT_HEIGHT ?? '660', 10),
+    viewportWidth: parseInt(process.env.VIEWPORT_WIDGTH ?? '1000', 10),
+    viewportHeight: parseInt(process.env.VIEWPORT_HEIGHT ?? '660', 10),
 });
