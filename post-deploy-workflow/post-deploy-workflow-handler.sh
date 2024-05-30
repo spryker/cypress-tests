52f87a5b0d3c7c9f397ea82c4b2af6927ef016e9
Denys Sokolov <denys.sokolov@spryker.com>
2024-05-30 15:53:36 +0200

    CC-33647: Fixed the workflow.

--- post-deploy-workflow/post-deploy-workflow-handler.sh
+++ post-deploy-workflow/post-deploy-workflow-handler.sh
@@ -2,12 +2,12 @@
 
 mkdir -p artifacts
 
-docker-compose -f vendor/spryker/cypress-tests/post-deploy-workflow/docker-compose.cypress-post-deploy-workflow.yml up -d
+docker-compose -f ../docker-compose.cypress-post-deploy-workflow.yml up -d
 
 # Run the cypress smoke tests in the cypress-tests service's container
 # If the tests fail, upload the artifacts to the specified AWS S3 bucket
-if ! docker-compose -f vendor/spryker/cypress-tests/post-deploy-workflow/docker-compose.cypress-post-deploy-workflow.yml exec -T cypress-tests bash -c "ENV_REPOSITORY_ID=$DEMO_SHOP_TYPE npm run $NPM_COMMAND"; then
-     aws s3 cp vendor/spryker/cypress-tests/artifacts s3://$ROBOT_TESTS_ARTIFACTS_BUCKET/cypress-ui/$DEMO_SHOP_TYPE/post-deploy/$CODEBUILD_BUILD_ID/ \
+if ! docker-compose -f ../docker-compose.cypress-post-deploy-workflow.yml exec -T cypress-tests bash -c "ENV_REPOSITORY_ID=$DEMO_SHOP_TYPE npm run $NPM_COMMAND"; then
+     aws s3 cp ../artifacts s3://$ROBOT_TESTS_ARTIFACTS_BUCKET/cypress-ui/$DEMO_SHOP_TYPE/post-deploy/$CODEBUILD_BUILD_ID/ \
                          --recursive \
                          --expires "$(date -d '+7 days' --utc +'%Y-%m-%dT%H:%M:%SZ')"
 
@@ -15,5 +15,5 @@ if ! docker-compose -f vendor/spryker/cypress-tests/post-deploy-workflow/docker-
     exit 1
 fi
 
-docker-compose -f vendor/spryker/cypress-tests/post-deploy-workflow/docker-compose.cypress-post-deploy-workflow.yml down
+docker-compose -f ../docker-compose.cypress-post-deploy-workflow.yml down
 rm -rf artifacts
