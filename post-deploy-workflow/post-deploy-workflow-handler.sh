53447061a54611457c873dae4ece57167acfed78
Denys Sokolov <denys.sokolov@spryker.com>
2024-06-07 09:44:32 +0200

    CC-33771: Fixed the setup of the "ENV_REPOSITORY_ID".

--- post-deploy-workflow/post-deploy-workflow-handler.sh
+++ post-deploy-workflow/post-deploy-workflow-handler.sh
@@ -7,7 +7,7 @@ docker-compose -f ../docker-compose.cypress-post-deploy-workflow.yml up -d
 # Run the cypress smoke tests in the cypress-tests service's container
 # If the tests fail, upload the artifacts to the specified AWS S3 bucket
 if ! docker-compose -f ../docker-compose.cypress-post-deploy-workflow.yml exec -T cypress-tests bash -c "ENV_REPOSITORY_ID=$ENV_REPOSITORY_ID npm run $NPM_COMMAND"; then
-     aws s3 cp ../artifacts s3://$ROBOT_TESTS_ARTIFACTS_BUCKET/cypress-ui/$DEMO_SHOP_TYPE/post-deploy/$CODEBUILD_BUILD_ID/ \
+     aws s3 cp ../artifacts s3://$ROBOT_TESTS_ARTIFACTS_BUCKET/cypress-ui/$ENV_REPOSITORY_ID/post-deploy/$CODEBUILD_BUILD_ID/ \
                          --recursive \
                          --expires "$(date -d '+7 days' --utc +'%Y-%m-%dT%H:%M:%SZ')"
 
