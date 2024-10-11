05f8237cbd7366dd5517ba5598a3cd2d6b1fe131
aleksandr-velikanov <aleksandr.velikanov>
2024-10-11 20:01:37 -0300

    FRW-8354 Adjust timeouts

--- cypress/support/pages/backoffice/store/create/store-create-repository.ts
+++ cypress/support/pages/backoffice/store/create/store-create-repository.ts
@@ -16,16 +16,19 @@ export class StoreCreateRepository {
   getLocaleSearchInput = (): Cypress.Chainable => cy.get('#available-locale-table_filter').find('input');
   getCurrencySearchInput = (): Cypress.Chainable => cy.get('#available-currency-table_filter').find('input');
   getAvailableLocaleInput = (locale: string): Cypress.Chainable =>
-    cy.get(`#available-locale-table_wrapper [value="${locale}"]`, { timeout: 15000 });
+    cy.get(`#available-locale-table_wrapper [value="${locale}"]`, { timeout: 10000 });
   getAvailableCurrencyInput = (currency: string): Cypress.Chainable =>
-    cy.get(`#available-currency-table [value="${currency}"]`);
+    cy.get(`#available-currency-table [value="${currency}"]`, { timeout: 10000 });
   getDisplayRegionsTab = (): Cypress.Chainable => cy.get('[data-tab-content-id="tab-content-country_store_relation"]');
   getCountrySearchInput = (): Cypress.Chainable => cy.get('#available-country-table_filter').find('input');
   getAvailableCountryInput = (country: string): Cypress.Chainable =>
-    cy.get(`#available-country-table [value="${country}"]`);
+    cy.get(`#available-country-table [value="${country}"]`, { timeout: 10000 });
   getSaveButton = (): Cypress.Chainable => cy.get('[type="submit"]');
 
-  getStoreContextTabButton = (): Cypress.Chainable => cy.get('[data-tab-content-id="tab-content-store_context"]', { timeout: 15000 });
-  getAddStoreContextButton = (): Cypress.Chainable => cy.get('#tab-content-store_context .btn.add-store-context', { timeout: 15000 });
-  getTimezoneSelector = (): Cypress.Chainable => cy.get('#store_applicationContextCollection_applicationContexts_1_timezone', { timeout: 25000 });
+  getStoreContextTabButton = (): Cypress.Chainable =>
+    cy.get('[data-tab-content-id="tab-content-store_context"]', { timeout: 15000 });
+  getAddStoreContextButton = (): Cypress.Chainable =>
+    cy.get('#tab-content-store_context .btn.add-store-context', { timeout: 15000 });
+  getTimezoneSelector = (): Cypress.Chainable =>
+    cy.get('#store_applicationContextCollection_applicationContexts_1_timezone', { timeout: 25000 });
 }
