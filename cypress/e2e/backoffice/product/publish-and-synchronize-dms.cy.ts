036fc451d0582b000adb3000d55d736744c3a6c8
annakotiuk <anna.kotiuk@spryker.com>
2024-10-27 22:30:31 +0100

    FRW-8354: debug

--- cypress/e2e/backoffice/product/publish-and-synchronize-dms.cy.ts
+++ cypress/e2e/backoffice/product/publish-and-synchronize-dms.cy.ts
@@ -47,8 +47,7 @@ import { CustomerLoginScenario, SelectStoreScenario } from '@scenarios/yves';
 
             cy.log('logged in');
 
-            productAbstract = createProductScenario.execute({shouldTriggerPublishAndSync: true});
-           
+
         });
 
         it('backoffice user should be able to create new product that will be available for guests in storefront', (): void => {
