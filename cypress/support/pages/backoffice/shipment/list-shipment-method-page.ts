b0fc1bebd5be1a827fa7b1655eea34beb7d737a0
annakotiuk <anna.kotiuk@spryker.com>
2024-10-28 16:09:40 +0100

    FRW-8354: debug

--- cypress/support/pages/backoffice/shipment/list-shipment-method-page.ts
+++ cypress/support/pages/backoffice/shipment/list-shipment-method-page.ts
@@ -15,9 +15,9 @@ export class ListShipmentMethodPage extends BackofficePage {
     cy.get(searchSelector).clear();
     cy.get(searchSelector).type(params.query);
 
-    this.interceptTable({ url: 'shipment-gui/shipment-method/table**', expectedCount: params.expectedCount });
-
-    return cy.get('tbody > tr:visible');
+    return this.interceptTable(
+        { url: 'shipment-gui/shipment-method/table**', expectedCount: params.expectedCount },
+        () => cy.get('tbody > tr:visible'));
   };
 }
 
