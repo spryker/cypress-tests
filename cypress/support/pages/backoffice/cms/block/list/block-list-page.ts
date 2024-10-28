5f428df2e65f448bb16948e0c9c82d42c5c867bf
annakotiuk <anna.kotiuk@spryker.com>
2024-10-28 16:58:24 +0100

    FRW-8354: debug

--- cypress/support/pages/backoffice/cms/block/list/block-list-page.ts
+++ cypress/support/pages/backoffice/cms/block/list/block-list-page.ts
@@ -6,35 +6,35 @@ import { BlockListRepository } from './block-list-repository';
 @injectable()
 @autoWired
 export class BlockListPage extends BackofficePage {
-  @inject(BlockListRepository) private repository: BlockListRepository;
+    @inject(BlockListRepository) private repository: BlockListRepository;
 
-  protected PAGE_URL = '/cms-block-gui/list-block';
+    protected PAGE_URL = '/cms-block-gui/list-block';
 
-  update = (params: UpdateParams): void => {
-    const findParams = { query: params.query, expectedCount: 1 };
+    update = (params: UpdateParams): void => {
+        const findParams = { query: params.query, expectedCount: 1 };
 
-    this.find(findParams).then(($storeRow) => {
-        cy.wrap($storeRow).as('row');
-        cy.get('@row').find(this.repository.getEditButtonSelector()).should('exist').click()
-    });
-  };
+        this.find(findParams).then(($storeRow) => {
+            cy.wrap($storeRow).as('row');
+            cy.get('@row').find(this.repository.getEditButtonSelector()).should('exist').click()
+        });
+    };
 
-  find = (params: FindParams): Cypress.Chainable => {
-    const searchSelector = this.repository.getSearchSelector();
-    cy.get(searchSelector).clear();
-    cy.get(searchSelector).type(params.query);
+    find = (params: FindParams): Cypress.Chainable => {
+        const searchSelector = this.repository.getSearchSelector();
+        cy.get(searchSelector).clear();
+        cy.get(searchSelector).type(params.query);
 
-    this.interceptTable({ url: 'cms-block-gui/list-block/table**', expectedCount: params.expectedCount });
-
-    return this.repository.getFirstTableRow();
-  };
+        return this.interceptTable(
+            { url: 'cms-block-gui/list-block/table**', expectedCount: params.expectedCount },
+            () => this.repository.getFirstTableRow());
+    };
 }
 
 interface FindParams {
-  query: string;
-  expectedCount?: number;
+    query: string;
+    expectedCount?: number;
 }
 
 interface UpdateParams {
-  query: string;
+    query: string;
 }
