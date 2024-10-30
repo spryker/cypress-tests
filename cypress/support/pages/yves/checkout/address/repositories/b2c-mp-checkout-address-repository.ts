4b647b58d103b8d0eb07169f2534c1d435a1ec21
aleksandr-velikanov <aleksandr.velikanov>
2024-10-30 20:55:07 -0300

    FRW-8354 Add missing method implementation

--- cypress/support/pages/yves/checkout/address/repositories/b2c-mp-checkout-address-repository.ts
+++ cypress/support/pages/yves/checkout/address/repositories/b2c-mp-checkout-address-repository.ts
@@ -69,4 +69,6 @@ export class B2cMpCheckoutAddressRepository implements CheckoutAddressRepository
     index: number
   ): Cypress.Chainable =>
     cy.wrap($addressItem).get(`#addressesForm_multiShippingAddresses_${index}_shippingAddress_phone`);
+
+  getFirstShipmentTypeRadio = (): Cypress.Chainable => cy.get('#addressesForm_shipmentType_key_0');
 }
