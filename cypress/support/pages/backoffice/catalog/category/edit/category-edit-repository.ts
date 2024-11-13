981e44b131805d000dadfc5734926f486772e74c
aleksandr-velikanov <aleksandr.velikanov>
2024-11-13 18:19:37 -0300

    FRW-6354 Reformat

--- cypress/support/pages/backoffice/catalog/category/edit/category-edit-repository.ts
+++ cypress/support/pages/backoffice/catalog/category/edit/category-edit-repository.ts
@@ -4,6 +4,6 @@ import { injectable } from 'inversify';
 @injectable()
 @autoWired
 export class CategoryEditRepository {
-    getStoreSelect = (): Cypress.Chainable => cy.get('#category_store_relation_id_stores');
-    getSaveButton = (): Cypress.Chainable => cy.get('button.safe-submit');
+  getStoreSelect = (): Cypress.Chainable => cy.get('#category_store_relation_id_stores');
+  getSaveButton = (): Cypress.Chainable => cy.get('button.safe-submit');
 }
