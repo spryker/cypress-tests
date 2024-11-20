1fb263ba7ed39afd64b775564687e407e6049d94
Dmytro Asieiev <dmiseev@gmail.com>
2024-11-20 13:46:13 +0300

    Clean-up unused code.

--- cypress/support/pages/backoffice/backoffice-page.ts
+++ cypress/support/pages/backoffice/backoffice-page.ts
@@ -30,23 +30,6 @@ export class BackofficePage extends AbstractPage {
       });
   };
 
-  public getEditButton = (params: UpdateParams): Cypress.Chainable => {
-    return this.find(params).then(($row) => {
-      if (!$row) {
-        cy.log('No rows found after filtering');
-        return null;
-      }
-      return this.getEditButtonFromRow($row).then(($button) => {
-        if ($button.length) {
-          return $button;
-        } else {
-          cy.log('Record is assigned to the store or not found by search criteria');
-          return null;
-        }
-      });
-    });
-  };
-
   public find = (params: UpdateParams): Cypress.Chainable => {
     // eslint-disable-next-line cypress/unsafe-to-chain-command
     return cy
@@ -77,59 +60,6 @@ export class BackofficePage extends AbstractPage {
         });
       });
   };
-
-  public findWithretry = (params: UpdateWithRetryParams): Cypress.Chainable => {
-    const retryCount = 2;
-    let attempts = 0;
-
-    const searchAndIntercept = (): Cypress.Chainable => {
-      attempts++;
-      // eslint-disable-next-line cypress/unsafe-to-chain-command
-      cy.get('[type="search"]')
-        .clear()
-        .then(() => {
-          cy.visitBackoffice(params.pageUrl);
-        });
-
-      return this.interceptTable({ url: params.tableUrl }).then(() => {
-        // eslint-disable-next-line cypress/unsafe-to-chain-command
-        cy.get('[type="search"]')
-          .invoke('val', params.searchQuery)
-          .trigger('input')
-          .then(() => {
-            return this.interceptTable({ url: params.tableUrl, expectedCount: params.expectedCount }, () => {
-              cy.get('tbody > tr:visible').then(($rows) => {
-                let rows = Cypress.$($rows);
-
-                if (params.rowFilter && params.rowFilter.length > 0) {
-                  params.rowFilter.forEach((filterFn) => {
-                    if (rows.length > 0) {
-                      rows = rows.filter((index, row) => filterFn(Cypress.$(row)));
-                    }
-                  });
-                }
-
-                if (rows.length > 0) {
-                  return cy.wrap(rows.first());
-                } else if (attempts < retryCount) {
-                  cy.log(`Retrying... Attempt ${attempts}`);
-                  return searchAndIntercept();
-                } else {
-                  cy.log('No rows found after filtering');
-                  return null;
-                }
-              });
-            });
-          });
-      });
-    };
-
-    return searchAndIntercept();
-  };
-
-  private getEditButtonFromRow = ($row: JQuery<HTMLElement>): Cypress.Chainable => {
-    return cy.wrap($row).find('a:contains("Edit")');
-  };
 }
 
 export enum ActionEnum {
@@ -153,12 +83,4 @@ interface UpdateParams {
   tableUrl: string;
   rowFilter?: Array<(row: JQuery<HTMLElement>) => boolean>;
   expectedCount?: number;
-}
-
-interface UpdateWithRetryParams {
-  searchQuery: string;
-  tableUrl: string;
-  rowFilter?: Array<(row: JQuery<HTMLElement>) => boolean>;
-  expectedCount?: number | null;
-  pageUrl: string;
 }
