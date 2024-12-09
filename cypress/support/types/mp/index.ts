9565cef2fefe246efbad3a9d83bee4f9731549f9
Dmytro Asieiev <dmiseev@gmail.com>
2024-12-09 22:26:39 +0300

    Added missing Order Management tests.

--- cypress/support/types/mp/index.ts
+++ cypress/support/types/mp/index.ts
@@ -3,3 +3,4 @@ export * from './merchant-b2b-contract-requests';
 export * from './marketplace-merchant-commission';
 export * from './marketplace-product-concretes';
 export * from './marketplace-products';
+export * from './marketplace-order-management';
