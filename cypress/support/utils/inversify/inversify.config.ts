50854848a87a3a528f73c352208ec44bc3238785
Mantas Muliarcikas <mantas.muliarcikas@spryker.com>
2025-02-07 13:12:39 +0200

    CC-34804 added b2b tests support

--- cypress/support/utils/inversify/inversify.config.ts
+++ cypress/support/utils/inversify/inversify.config.ts
@@ -122,6 +122,8 @@ import { SuiteCompanyRoleIndexRepository } from '../../pages/yves/company/compan
 import { SuiteCompanyRoleCreateRepository } from '../../pages/yves/company/company-role/create/repositories/suite-company-role-create-repository';
 import { B2bMpCompanyRoleCreateRepository } from '../../pages/yves/company/company-role/create/repositories/b2b-mp-company-role-create-repository';
 import { B2bMpCompanyRoleIndexRepository } from '../../pages/yves/company/company-role/index/repositories/b2b-mp-company-role-index-repository';
+import { B2bCompanyRoleIndexRepository } from '../../pages/yves/company/company-role/index/repositories/b2b-company-role-index-repository';
+import { B2bCompanyRoleCreateRepository } from '../../pages/yves/company/company-role/create/repositories/b2b-company-role-create-repository';
 
 type BindingsMap = { [K in REPOSITORIES]?: interfaces.Newable<unknown> };
 
@@ -203,6 +205,8 @@ const b2bMappings: BindingsMap = {
   [REPOSITORIES.OrderDetailsRepository]: B2bOrderDetailsRepository,
   [REPOSITORIES.ContentRepository]: B2bContentRepository,
   [REPOSITORIES.ProductComparisonRepository]: B2bProductComparisonRepository,
+  [REPOSITORIES.CompanyRoleIndexRepository]: B2bCompanyRoleIndexRepository,
+  [REPOSITORIES.CompanyRoleCreateRepository]: B2bCompanyRoleCreateRepository,
 };
 
 const b2cMpMappings: BindingsMap = {
