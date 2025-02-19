ef2890ae0bb49b9c4f00415f887c0cf1266a54a6
Mantas Muliarcikas <mantas.muliarcikas@spryker.com>
2025-02-19 14:44:05 +0200

    CC-34804 added b2c filtering for Backoffice company role tests

--- cypress/e2e/backoffice/company-role/company-role-create.cy.ts
+++ cypress/e2e/backoffice/company-role/company-role-create.cy.ts
@@ -5,29 +5,31 @@ import { UserLoginScenario } from '@scenarios/backoffice';
 
 (['b2c', 'b2c-mp'].includes(Cypress.env('repositoryId')) ? describe.skip : describe)(
   'company role create',
-  { tags: ['@backoffice', '@company-role'] }, (): void => {
-  const companyRoleListPage = container.get(CompanyRoleListPage);
-  const companyRoleCreatePage = container.get(CompanyRoleCreatePage);
-  const userLoginScenario = container.get(UserLoginScenario);
+  { tags: ['@backoffice', '@company-role'] },
+  (): void => {
+    const companyRoleListPage = container.get(CompanyRoleListPage);
+    const companyRoleCreatePage = container.get(CompanyRoleCreatePage);
+    const userLoginScenario = container.get(UserLoginScenario);
 
-  let staticFixtures: CompanyRoleCreateStaticFixtures;
-  let dynamicFixtures: CompanyRoleCreateDynamicFixtures;
+    let staticFixtures: CompanyRoleCreateStaticFixtures;
+    let dynamicFixtures: CompanyRoleCreateDynamicFixtures;
 
-  before((): void => {
-    ({ dynamicFixtures, staticFixtures } = Cypress.env());
-  });
-
-  beforeEach((): void => {
-    userLoginScenario.execute({
-      username: dynamicFixtures.rootUser.username,
-      password: staticFixtures.defaultPassword,
+    before((): void => {
+      ({ dynamicFixtures, staticFixtures } = Cypress.env());
     });
-  });
 
-  it('should be able to access company role create page', (): void => {
-    companyRoleListPage.visit();
-    companyRoleListPage.clickAddCompanyUserRoleButton();
-    companyRoleCreatePage.assertPageLocation();
-    companyRoleCreatePage.getCompanyRoleCreateForm().should('exist').and('be.visible');
-  });
-});
+    beforeEach((): void => {
+      userLoginScenario.execute({
+        username: dynamicFixtures.rootUser.username,
+        password: staticFixtures.defaultPassword,
+      });
+    });
+
+    it('should be able to access company role create page', (): void => {
+      companyRoleListPage.visit();
+      companyRoleListPage.clickAddCompanyUserRoleButton();
+      companyRoleCreatePage.assertPageLocation();
+      companyRoleCreatePage.getCompanyRoleCreateForm().should('exist').and('be.visible');
+    });
+  }
+);
