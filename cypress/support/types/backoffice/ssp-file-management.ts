02c1f9b117d8745cf58a5dc21f150237b06c104d
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2025-02-21 13:53:01 +0200

    SSP-2: fix CI

--- cypress/support/types/backoffice/ssp-file-management.ts
+++ cypress/support/types/backoffice/ssp-file-management.ts
@@ -1,12 +1,9 @@
 import { User } from './shared';
-import { File, FileAttachment } from '@interfaces/shared';
 
 export interface SspFileManagementStaticFixtures {
   defaultPassword: string;
 }
 
 export interface SspFileManagementDynamicFixtures {
-  file: File;
-  fileAttachment: FileAttachment;
   rootUser: User;
 }
