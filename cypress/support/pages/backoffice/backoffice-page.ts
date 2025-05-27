e0a92f99ea9c49f0f789c5bb104533956de9716d
Damir Zakirov <damirzakirov@damir-zakirov.home>
2025-05-27 14:08:56 +0200

    CC-35706 reverted changes

--- cypress/support/pages/backoffice/backoffice-page.ts
+++ cypress/support/pages/backoffice/backoffice-page.ts
@@ -33,7 +33,7 @@ export class BackofficePage extends AbstractPage {
   public find = (params: UpdateParams): Cypress.Chainable => {
     // eslint-disable-next-line cypress/unsafe-to-chain-command
     return cy
-      .get('[type="search"][aria-controls^="table-"]')
+      .get('input[type="search"][data-qa="table-search"]')
       .clear()
       .invoke('val', params.searchQuery)
       .trigger('input')
@@ -68,7 +68,7 @@ export class BackofficePage extends AbstractPage {
     const searchAndIntercept = (): Cypress.Chainable => {
       attempts++;
       // eslint-disable-next-line cypress/unsafe-to-chain-command
-      cy.get('[type="search"][aria-controls^="table-"]')
+      cy.get('input[type="search"][data-qa="table-search"]')
         .clear()
         .then(() => {
           cy.visitBackoffice(params.pageUrl);
@@ -76,7 +76,7 @@ export class BackofficePage extends AbstractPage {
 
       return this.interceptTable({ url: params.tableUrl }).then(() => {
         // eslint-disable-next-line cypress/unsafe-to-chain-command
-        cy.get('[type="search"][aria-controls^="table-"]')
+        cy.get('input[type="search"][data-qa="table-search"]')
           .invoke('val', params.searchQuery)
           .trigger('input')
           .then(() => {
