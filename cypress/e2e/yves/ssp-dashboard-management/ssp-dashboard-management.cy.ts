c9d4afa8060c2513fb61cc8be7107d5955c96cb8
annakotiuk <anna.kotiuk@spryker.com>
2025-07-22 16:55:17 +0200

    SSP-1333: CR fixes

--- cypress/e2e/yves/ssp-dashboard-management/ssp-dashboard-management.cy.ts
+++ cypress/e2e/yves/ssp-dashboard-management/ssp-dashboard-management.cy.ts
@@ -75,7 +75,8 @@ import { CustomerLoginScenario } from '@scenarios/yves';
       sspDashboardPage.assertSspDashboardFilesBlockNotPresent();
     });
 
-      it('customer should see empty files block on dashboard', (): void => {
+    !isB2B
+      ? it('customer should see empty files block on dashboard', (): void => {
           customerLoginScenario.execute({
             email: dynamicFixtures.customer3.email,
             password: staticFixtures.defaultPassword,
@@ -85,7 +86,8 @@ import { CustomerLoginScenario } from '@scenarios/yves';
           sspDashboardPage.visit();
           sspDashboardPage.assertSspDashboardFilesBlockPresent();
           sspDashboardPage.assertSspDashboardFilesTableEmpty();
-        });
+        })
+      : it.skip('customer should see empty files block on dashboard');
 
     it('customer should see files on dashboard', (): void => {
       customerLoginScenario.execute({
@@ -125,7 +127,8 @@ import { CustomerLoginScenario } from '@scenarios/yves';
       sspDashboardPage.assertSspDashboardInquiriesBlockNotPresent();
     });
 
-      it('customer without permission see empty inquiries table on dashboard', (): void => {
+    !isB2B
+      ? it('customer without permission see empty inquiries table on dashboard', (): void => {
           customerLoginScenario.execute({
             email: dynamicFixtures.customer4.email,
             password: staticFixtures.defaultPassword,
@@ -135,7 +138,8 @@ import { CustomerLoginScenario } from '@scenarios/yves';
           sspDashboardPage.visit();
           sspDashboardPage.assertSspDashboardInquiriesBlockPresent();
           sspDashboardPage.assertSspDashboardInquiriesTableEmpty();
-        });
+        })
+      : it.skip('customer without permission see empty inquiries table on dashboard');
 
     it('customer should see inquiries on dashboard', (): void => {
       customerLoginScenario.execute({
