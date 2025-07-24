9c3aa4a6213ff9d1e7c8a9aaee521c119ea9a4cc
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2025-07-24 15:41:09 +0300

    SSP bugfixes.

--- cypress/support/pages/yves/ssp-dashboard-management/repositories/b2b-ssp-dashboard-management-repository.ts
+++ cypress/support/pages/yves/ssp-dashboard-management/repositories/b2b-ssp-dashboard-management-repository.ts
@@ -66,7 +66,7 @@ export class B2bSspDashboardManagementRepository implements SspDashboardManageme
     return 'No Files';
   }
   getFilesHeaders(): string[] {
-    return ['File Name', 'Size', 'File Type', ''];
+    return ['File Name', 'Size', 'File Format', ''];
   }
   getInquiriesBlock(): Chainable {
     return cy.get('[data-qa="component dashboard-table ssp-inquiry-table"]');
