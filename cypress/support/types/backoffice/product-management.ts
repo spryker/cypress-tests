26f1dc43b3e8ca0d16c7a48612d42ea9ada47fdf
Damir Zakirov <damir.zakirov@spryker.com>
2025-08-01 13:44:55 +0200

    CC-36099 adjusted ProductManagementDynamicFixtures

--- cypress/support/types/backoffice/product-management.ts
+++ cypress/support/types/backoffice/product-management.ts
@@ -1,13 +1,12 @@
-import { User, ProductClass, Product, Store } from './shared';
+import { User, Product, Store } from './shared';
 
 export interface ProductManagementStaticFixtures {
   defaultPassword: string;
 }
 
 export interface ProductManagementDynamicFixtures {
-  rootUser: User;
-  productClass: ProductClass;
-  product: Product;
   storeAT: Store;
   storeDE: Store;
+  rootUser: User;
+  product: Product;
 }
