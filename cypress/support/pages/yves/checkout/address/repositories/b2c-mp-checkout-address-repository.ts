c4ac7111ee404437287705be9dc95e074dbec59c
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2025-08-04 19:32:03 +0200

    SSP-667 Fixed cypress tests typecheck.

--- cypress/support/pages/yves/checkout/address/repositories/b2c-mp-checkout-address-repository.ts
+++ cypress/support/pages/yves/checkout/address/repositories/b2c-mp-checkout-address-repository.ts
@@ -71,4 +71,6 @@ export class B2cMpCheckoutAddressRepository implements CheckoutAddressRepository
     index: number
   ): Cypress.Chainable =>
     cy.wrap($addressItem).get(`#addressesForm_multiShippingAddresses_${index}_shippingAddress_phone`);
+  getShipmentTypeRadio = (shipmentType: string): Cypress.Chainable => 
+    cy.get(`[data-qa="shipment-type-radio"][value="${shipmentType}"]`);
 }
