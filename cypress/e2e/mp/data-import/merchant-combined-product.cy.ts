e2ca92899614ad0685ccf1364d53049ffcd21e8c
Dmytro Tsybenko <dmytro.tsybenko@spryker.com>
2025-08-14 16:54:14 +0300

    CC-36137: added test steps and file for uploading

--- cypress/e2e/mp/data-import/merchant-combined-product.cy.ts
+++ cypress/e2e/mp/data-import/merchant-combined-product.cy.ts
@@ -16,10 +16,33 @@ import { MerchantCombinedProductDynamicFixtures, MerchantCombinedProductStaticFi
     });
 
     it('merchant can start import', (): void => {
-      merchantUserLoginScenario.execute({
-        username: dynamicFixtures.merchantUser.username,
-        password: staticFixtures.defaultPassword,
-      });
+      const repositoryId = Cypress.env('repositoryId');
+
+      cy.readFile('cypress/fixtures/' + repositoryId + '/mp/data-import/merchant_combined_product-1.csv').then(
+        (contents) => {
+          const timestamp: number = Date.now();
+
+          contents = contents.replaceAll('{UNIQUE}', timestamp);
+
+          const file: Cypress.FileReference = {
+            fileName: 'merchant_combined_product.csv',
+            contents: Cypress.Buffer.from(contents),
+            mimeType: 'text/csv',
+            lastModified: timestamp,
+          };
+
+          merchantUserLoginScenario.execute({
+            username: dynamicFixtures.merchantUser.username,
+            password: staticFixtures.defaultPassword,
+          });
+          cy.visit('http://mp.eu.spryker.local/file-import-merchant-portal-gui/history');
+          cy.contains('Start Import').click();
+          cy.get('select[name="merchant_file_import_form[entity_type]"]').select('Product', { force: true });
+          cy.get('input[name="merchant_file_import_form[merchantFile][file]"]').selectFile(file);
+          cy.get('web-spy-button').contains('Import').click();
+          cy.get('body').should('contain', 'File import has been started');
+        }
+      );
     });
   }
 );
