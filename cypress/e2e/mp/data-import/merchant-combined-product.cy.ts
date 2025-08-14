172f0444e740dc324812117e3a21987a699c27e5
Dmytro Tsybenko <dmytro.tsybenko@spryker.com>
2025-08-14 19:08:15 +0300

    CC-36137: added test steps for product approval in BO and validation in Storefront

--- cypress/e2e/mp/data-import/merchant-combined-product.cy.ts
+++ cypress/e2e/mp/data-import/merchant-combined-product.cy.ts
@@ -1,12 +1,18 @@
 import { container } from '@utils';
 import { MerchantUserLoginScenario } from '@scenarios/mp';
+import { UserLoginScenario } from '@scenarios/backoffice';
+import { ActionEnum, ProductManagementListPage } from '@pages/backoffice';
 import { MerchantCombinedProductDynamicFixtures, MerchantCombinedProductStaticFixtures } from '@interfaces/mp';
+import { CatalogPage } from '@pages/yves';
 
 (['suite', 'b2b-mp', 'b2c-mp'].includes(Cypress.env('repositoryId')) ? describe : describe.skip)(
   'merchant combined product data import',
   { tags: ['@mp', '@data-import'] },
   (): void => {
+    const catalogPage = container.get(CatalogPage);
     const merchantUserLoginScenario = container.get(MerchantUserLoginScenario);
+    const userLoginScenario = container.get(UserLoginScenario);
+    const productManagementListPage = container.get(ProductManagementListPage);
 
     let dynamicFixtures: MerchantCombinedProductDynamicFixtures;
     let staticFixtures: MerchantCombinedProductStaticFixtures;
@@ -21,6 +27,7 @@ import { MerchantCombinedProductDynamicFixtures, MerchantCombinedProductStaticFi
       cy.readFile('cypress/fixtures/' + repositoryId + '/mp/data-import/merchant_combined_product-1.csv').then(
         (contents) => {
           const timestamp: number = Date.now();
+          const abstractSku: string = 'PRODUCT-' + timestamp;
 
           contents = contents.replaceAll('{UNIQUE}', timestamp);
 
@@ -31,6 +38,7 @@ import { MerchantCombinedProductDynamicFixtures, MerchantCombinedProductStaticFi
             lastModified: timestamp,
           };
 
+          // Start import in Merchant Portal
           merchantUserLoginScenario.execute({
             username: dynamicFixtures.merchantUser.username,
             password: staticFixtures.defaultPassword,
@@ -41,6 +49,22 @@ import { MerchantCombinedProductDynamicFixtures, MerchantCombinedProductStaticFi
           cy.get('input[name="merchant_file_import_form[merchantFile][file]"]').selectFile(file);
           cy.get('web-spy-button').contains('Import').click();
           cy.get('body').should('contain', 'File import has been started');
+
+          cy.runCliCommands(['console merchant-portal:file-import']);
+
+          // Approve product in the backoffice
+          userLoginScenario.execute({
+            username: dynamicFixtures.rootUser.username,
+            password: staticFixtures.defaultPassword,
+          });
+          productManagementListPage.visit();
+          productManagementListPage.update({ query: abstractSku, action: ActionEnum.approve });
+          cy.runCliCommands(['console queue:worker:start --stop-when-empty']);
+
+          // Check that product is accessible in Storefront
+          catalogPage.visit();
+          catalogPage.searchProductFromSuggestions({ query: abstractSku });
+          cy.contains(abstractSku);
         }
       );
     });
