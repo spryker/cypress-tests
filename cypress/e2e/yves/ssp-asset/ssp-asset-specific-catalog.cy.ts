9300193e6923f3fc689cd06dfd85aa5828a7c317
annakotiuk <anna.kotiuk@spryker.com>
2025-09-11 16:35:04 +0200

    SSP-1176: create test for preseving asset

--- cypress/e2e/yves/ssp-asset/ssp-asset-specific-catalog.cy.ts
+++ cypress/e2e/yves/ssp-asset/ssp-asset-specific-catalog.cy.ts
@@ -1,5 +1,5 @@
 import { container } from '@utils';
-import { SspAssetDetailPage, CatalogPage } from '@pages/yves';
+import { SspAssetDetailPage, CatalogPage, ProductPage } from '@pages/yves';
 import { CustomerLoginScenario } from '@scenarios/yves';
 import { SspAssetSpecificCatalogStaticFixtures, SspAssetSpecificCatalogDynamicFixtures } from '@interfaces/yves';
 
@@ -12,6 +12,7 @@ import { SspAssetSpecificCatalogStaticFixtures, SspAssetSpecificCatalogDynamicFi
     const customerLoginScenario = container.get(CustomerLoginScenario);
     const sspAssetDetailPage = container.get(SspAssetDetailPage);
     const catalogPage = container.get(CatalogPage);
+    const productPage = container.get(ProductPage);
 
     before(() => {
       ({ staticFixtures, dynamicFixtures } = Cypress.env());
@@ -64,6 +65,10 @@ import { SspAssetSpecificCatalogStaticFixtures, SspAssetSpecificCatalogDynamicFi
         .first()
         .should('contain', dynamicFixtures.productSpareParts.localized_attributes[0].name);
       catalogPage.getSspAssetNameBlock().should('contain', dynamicFixtures.sspAsset.name);
+
+      catalogPage.getProductItemBlocks().first().click();
+
+      productPage.getSspAssetNameBlock().should('contain', dynamicFixtures.sspAsset.name);
     });
 
     it('should view the catalog page with ssp asset service products', () => {
