ecf5217446894f6b1f4e6c30bb5378cdff0d3a64
krivtsova <olena.krivtsova@spryker.com>
2025-10-07 10:08:08 +0300

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -30,15 +30,20 @@ export default defineConfig({
     glueBackendUrl: `${protocol}://${glueBackendHost}`,
     glueStorefrontUrl: `${protocol}://${glueStorefrontHost}`,
     mailCatcherUrl: `${protocol}://${mailCatcherHost}`,
+    grepFilterSpecs: true,  
+    grepOmitFiltered: true,  
+    grepUntagged: true
   },
   e2e: {
     baseUrl: `${protocol}://${baseHost}`,
-    setupNodeEvents(on) {
+    setupNodeEvents(on,config) {
       on('task', {
         isFileExists(filename: string): boolean {
           return existsSync(filename);
         },
       });
+    require('@cypress/grep/src/plugin')(config);
+    return config;
     },
     retries: {
       runMode: 2,
