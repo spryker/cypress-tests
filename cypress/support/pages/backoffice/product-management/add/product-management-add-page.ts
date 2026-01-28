e8dca175e9e4f04ed4315eaec7bcc462028a00f9
Maksym Fedets <maksym.fedets@spryker.com>
2026-01-28 09:53:19 +0200

    fix: add body click to dismiss potential date picker after entering 'New From' date.

--- cypress/support/pages/backoffice/product-management/add/product-management-add-page.ts
+++ cypress/support/pages/backoffice/product-management/add/product-management-add-page.ts
@@ -26,6 +26,7 @@ export class ProductManagementAddPage extends BackofficePage {
     this.repository.getProductNameENInput().type(productAbstract.name);
     this.repository.getProductDescriptionENInput().type(productAbstract.description);
     this.repository.getNewFromInput().type(this.getCurrentDate());
+    cy.get('body').click(0, 0);
     this.repository.getNewToInput().type(this.getPlusOneMonthDate());
 
     this.repository.getPriceTaxTab().click();
