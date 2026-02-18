17a24966415b34ab2f9dcc4bdd4ce3cc02b0b54d
Maksym Fedets <maksym.fedets@spryker.com>
2026-02-18 18:12:49 +0200

    refactor: Update search input selectors and add timeouts for improved reliability
    - Refactored search input selectors across multiple repositories to use consistent data attributes.
    - Added a 5-second timeout to the agent merchant checkbox selector in user creation and update repositories for enhanced stability.

--- cypress/support/pages/backoffice/user/update/user-update-repository.ts
+++ cypress/support/pages/backoffice/user/update/user-update-repository.ts
@@ -4,7 +4,7 @@ import { injectable } from 'inversify';
 @injectable()
 @autoWired
 export class UserUpdateRepository {
-  getAgentMerchantCheckbox = (): Cypress.Chainable => cy.get('#user_is_merchant_agent');
+  getAgentMerchantCheckbox = (): Cypress.Chainable => cy.get('#user_is_merchant_agent', { timeout: 5000 });
   getAgentCustomerCheckbox = (): Cypress.Chainable => cy.get('#user_is_agent');
   getUpdateUserButton = (): Cypress.Chainable => cy.get('form[name=user]').find('[type="submit"]');
   getPasswordInput = (): Cypress.Chainable => cy.get('#user_password_first');
