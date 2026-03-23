e4f2f30ac60472ee12a9987448cb448099904160
Veronika Golska <veronica.golska@spryker.com>
2026-03-23 14:35:03 +0100

    CC-36760 restore user workflow integration

--- cypress/support/pages/yves/repositories/b2b-mp-yves-repository.ts
+++ cypress/support/pages/yves/repositories/b2b-mp-yves-repository.ts
@@ -10,5 +10,5 @@ export class B2bMpYvesRepository implements YvesRepository {
       .select(localeName.toUpperCase(), { force: true });
   getLocaleOptionsSelector = (): string => `[data-qa="language-selector"] option`;
   getLocaleAttributeName = (): string => 'data-locale';
-  getLastVisitedPageCookieName = (): string => '';
+  getLastVisitedPageCookieName = (): string => 'last-visited-page';
 }
