74d6c7067db446846eea5abf7c608e7c921d611c
denyssokolov <denys.sokolov@spryker.com>
2026-05-20 12:00:00 +0200

    CC-38071: Refactor table row selectors in budget and cost center repositories to use consistent data attributes for improved maintainability.

--- cypress/support/pages/yves/purchasing-control/budget/list/repositories/suite-budget-list-repository.ts
+++ cypress/support/pages/yves/purchasing-control/budget/list/repositories/suite-budget-list-repository.ts
@@ -5,7 +5,7 @@ import { BudgetListRepository } from '../budget-list-repository';
 export class SuiteBudgetListRepository implements BudgetListRepository {
   getCreateButton = (): Cypress.Chainable => cy.get('a[href*="budget/create"]');
 
-  getTableRows = (): Cypress.Chainable => cy.get('[data-qa="component data-table"] tbody tr');
+  getTableRows = (): Cypress.Chainable => cy.get('[data-qa="component advanced-table"] tbody tr');
 
   getEditButtonByUuid = (uuid: string): Cypress.Chainable =>
     cy.get(`[data-qa="cell-actions"] a[href*="${uuid}"]`).first();
