5bb4be5cde09dba91f31034361896c17479ad142
krivtsova <olena.krivtsova@spryker.com>
2026-05-27 10:26:29 +0300

    Update shop-theme.ts

--- cypress/support/types/smoke/shop-theme.ts
+++ cypress/support/types/smoke/shop-theme.ts
@@ -1,10 +1,3 @@
-export * from './checkout';
-export * from './customer-account-management';
-export * from './catalog';
-export * from './order-management';
-export * from './product';
-export * from './merchant-portal';
-export * from './ssp';
 
 export interface ShopThemeSmokeStaticFixtures {
   defaultPassword: string;
